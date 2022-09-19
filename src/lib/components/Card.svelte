1b9d29d34855f350a87d3dd0b89b7400a09d4a43
DaanKorver <daank2000@hotmail.nl>
2022-09-19 13:01:17 +0200

    Components

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+	//Dit zijn props (properties) van de component
+	//Deze kan je gebruiken om een component uniek te maken
+	//Je kan props gebruiken voor content, maar denk ook aan links (href), image paths of booleans (true / false)
+	export let title;
+	export let text;
+</script>
+
+<section>
+	<h3>{title}</h3>
+	<p>{text}</p>
+</section>
+
+<style>
+	section {
+		width: 25rem;
+		height: auto;
+		padding: 1em;
+		background-color: blue;
+		margin-bottom: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '../lib/components/Card.svelte';
+
+	//Deze data zou bijvoorbeeld uit een database kunnen komen (Prismic.io)
+	const kaarten = [
+		{
+			title: 'Eerste kaart',
+			text: 'Tweede'
+		},
+		{
+			title: 'Tweede kaart',
+			text: 'Fancy'
+		},
+		{
+			title: 'Drieeeee',
+			text: 'Loops zijn awesome'
+		}
+	];
+</script>
+
+<h1>Components</h1>
+<!-- Je kan components nu hergebruiken met verschillende content -->
+<Card title={'Kaart nummer een!'} text={'Hier kan tekst komen'} />
+<Card title={'Twee!'} text={'Hier kan een andere tekst'} />
+<Card title={'Nummer drie!'} text={'Tekst van de 3de'} />
+<Card title={'Nummertje 4'} text={'Laatste kaartje'} />
+
+<h2>Met een loop</h2>
+<!-- Je kan ook een loop gebruiken (dit kan handig zijn als je data krijg van een) -->
+{#each kaarten as kaart}
+	<Card title={kaart.title} text={kaart.text} />
+{/each}
